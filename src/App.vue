ff5536294437b3a10bbda4e00c4690bf58b0964f
Vladislav Haidukevich <vladislav.gajdukevich@gmail.com>
2021-09-26 19:36:15 +0300

    Получение первых постов с JSONPlaceholder и их вывод

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <list-post/>
+    </div>
+</template>
+
+<script>
+import ListPost from '@/component/ListPost.vue'
+import axios from 'axios'
+
+export default {
+    components: { ListPost, axios },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/component/ListPost.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="listpost" v-for="post in posts" :key="post.id">
+      <div><strong>Заголовок: </strong>{{post.title}}</div>
+      <div><strong>Текст: </strong>{{post.body}}</div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+     data() {
+        return {
+            posts: [{}],
+        }
+    },
+    methods: {
+      async getPosts(){
+        try{
+            const response = await axios.get("https://jsonplaceholder.typicode.com/posts?_limit=10");
+            this.posts = response.data;
+        }
+        catch(exeption){
+            console.log(exeption);
+        }
+        finally{
+
+        }
+      }
+    },
+    mounted(){  
+        this.getPosts()
+    }
+
+}
+</script>
+
+<style>
+.listpost{
+    margin-top: 5px;
+    background-color: rgb(240, 240, 240);
+    padding: auto;
+    border-block: solid;
+    border-block-color: black;
+}
+</style>
